3979cf5b41219c93f8968a3b25a95efc40f772c4
Zohaib Hasan <zohaib@codingcrust.com>
2022-07-15 11:20:16 +0500

    new changing

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         $this->call(GeneralSettingSeeder::class);
-//         $this->call(UserSeeder::class);
-        factory('App\User', 500)->create();
+         $this->call(GeneralSettingSeeder::class);
+         $this->call(UserSeeder::class);
+//        factory('App\User', 500)->create();
 //        factory('App\SendMessage', 100)->create();
     }
 }
